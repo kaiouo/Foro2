1ddc14f9310a4602bcc36e14ae4d9029096c4972
Katherine Beltrán <verme.ouo@gmail.com>
2024-03-16 13:19:01 -0600

    Update Tablet.java

--- src/com/poogroup/InheritedClasses/Tablet.java
+++ src/com/poogroup/InheritedClasses/Tablet.java
@@ -5,54 +5,93 @@ import com.poogroup.Interfaces.Equipo;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 public class Tablet implements Equipo {
 
     List<Equipo> tabletList = new ArrayList<>();
     private String fabricante;
     private String modelo;
     private String microprocesador;
-    private String memoria;
-    private String tarjetaGrafica;
 
-    public Tablet(String fabricante, String modelo, String microprocesador, String memoria, String tarjetaGrafica){
+    private String taPantalla;
+    private String capacitiva;
+    private String memoria;
+    private String sistemaOp;
+
+    public Tablet(String fabricante, String modelo, String microprocesador, String taPantalla, String capacitiva, String memoria, String sistemaOp){
         this.fabricante = fabricante;
         this.modelo = modelo;
         this.microprocesador = microprocesador;
+        this.taPantalla = taPantalla;
+        this.sistemaOp = capacitiva;
         this.memoria = memoria;
-        this.tarjetaGrafica = tarjetaGrafica;
+        this.sistemaOp = sistemaOp;
+        
     }
     @Override
     public String getFabricante() {
-        return null;
+        return "fabricante";
     }
 
     @Override
     public String getModelo() {
-        return null;
+        return "modelo";
     }
 
     @Override
     public String getMicroprocesador() {
-        return null;
+        return "microprocesador";
+    }
+   
+    public String getTaPantalla() {
+        return "taPantalla";
     }
 
+    
+    public String getCapacitiva() {
+        return "capacitiva";
+    }
+
+ 
     @Override
     public String getMemoria() {
-        return null;
+        return "memoria";
     }
 
-    @Override
-    public String getTarjetaGrafica() {
-        return null;
+ 
+  
+    public String getSistemaOp() {
+        return "sistemaOp";
     }
+
+
+ 
 
     @Override
     public void registrarAsset() {
+
+        fabricante=JOptionPane.showInputDialog("Ingrese el Fabricante");
+        modelo=JOptionPane.showInputDialog("Ingrese el modelo");
+        microprocesador=JOptionPane.showInputDialog("Ingrese el microprocesador");
+        taPantalla=JOptionPane.showInputDialog("Ingrese el tamaño diagonal de pantalla");
+        capacitiva=JOptionPane.showInputDialog("Ingrese la capacitiva o resistiva");
+        memoria=JOptionPane.showInputDialog("Ingrese el tamaño de memoria NAND");
+        sistemaOp=JOptionPane.showInputDialog("Ingrese el sistema operativo");
+        
 
     }
 
     @Override
     public void mostrarAssets() {
+
+        System.out.println("Fabricante: " +fabricante);
+        System.out.println("Modelo: " +modelo);
+        System.out.println("Microprocesador: " +microprocesador);
+        System.out.println("Tamaño diagonal de pantalla: " +taPantalla);
+        System.out.println("la capacitiva o resistiva: " +capacitiva);
+        System.out.println("tamaño de memoria NAND" +memoria);
+        System.out.println("Sistema Operativo: " +sistemaOp);
 
     }
 }
